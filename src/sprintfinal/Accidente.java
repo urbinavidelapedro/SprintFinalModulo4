881ad67a690edf25381c231ec8d142fd7566768e
Galtor-program <felipetorog@gmail.com>
2023-05-24 19:04:09 -0400

    Se agrega la clase accidente y se agrega prueba en Main

---
+++ src/sprintfinal/Accidente.java
@@ -0,0 +1,118 @@
+package sprintfinal;
+
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Scanner;
+import java.util.Date;
+
+
+public class Accidente {
+	private int idAccidente;
+	private String rutCliente, lugar, origen, consecuencias;
+	private LocalDate dia;
+	private LocalTime hora;
+	Scanner sc =  new Scanner(System.in);
+	
+	public Accidente(int idAccidente, String rutCliente, LocalDate dia, LocalTime hora, 
+		String lugar, String origen, String consecuencias) {
+		
+		this.idAccidente = idAccidente;
+		this.rutCliente = rutCliente;
+	
+		this.dia = dia;
+		this.hora = hora;
+				
+		while(lugar.isEmpty()) {
+			System.out.println("Ingrese un lugar válido");
+			lugar = sc.nextLine();
+		}
+		while(lugar.length()<10 || lugar.length()>50) {
+			System.out.println("Ingrese un lugar de minimo 10 caracteres y maximo 50");
+			lugar = sc.nextLine();
+			
+		}
+		this.lugar = lugar;
+		
+		while (origen.length()>100 || origen.isEmpty()) {
+			System.out.println("Ingrese el Origen en menos de 100 caracteres");
+			origen = sc.nextLine();
+		}
+		this.origen = origen;
+		
+		while (consecuencias.length()>100 || consecuencias.isEmpty()) {
+			System.out.println("Ingrese las consecuencias en menos de 100 caracteres");
+			consecuencias = sc.nextLine();
+		}
+		this.consecuencias = consecuencias;
+		
+		
+	}
+
+	public Accidente() {
+		super();
+	}
+
+	public int getIdAccidente() {
+		return idAccidente;
+	}
+
+	public void setIdAccidente(int idAccidente) {
+		this.idAccidente = idAccidente;
+	}
+
+	public String getRutCliente() {
+		return rutCliente;
+	}
+
+	public void setRutCliente(String rutCliente) {
+		this.rutCliente = rutCliente;
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(String lugar) {
+		this.lugar = lugar;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getConsecuencias() {
+		return consecuencias;
+	}
+
+	public void setConsecuencias(String consecuencias) {
+		this.consecuencias = consecuencias;
+	}
+
+	public LocalDate getDia() {
+		return dia;
+	}
+
+	public void setDia(LocalDate dia) {
+		this.dia = dia;
+	}
+
+	public LocalTime getHora() {
+		return hora;
+	}
+
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+	
+	
+	
+
+
+	
+	
+}
--- src/sprintfinal/Main.java
+++ src/sprintfinal/Main.java
@@ -1,7 +1,15 @@
 package sprintfinal;
 
-public class Main {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
+public class Main {
+	
+	
+	
 	public static void main(String[] args) {
 
 		
@@ -12,7 +20,10 @@ public class Main {
 		Administrativo administrativo1=new Administrativo("Carla","02/02/1992","164264354","TI","Alcaldesa");
 		Administrativo administrativo2=new Administrativo("Carlos","04/06/1990","145615456","Contabilidad","Presidente");
 		Cliente cliente1=new Cliente("Max","10/12/2016","4561234856");
+		Accidente accidente1 = new Accidente(12354, "13513511", LocalDate.of(1997, 5, 12),LocalTime.of(5, 0),
+				"En un lugar lejano de toda civilizacion",	"", "Murio" );
 		
+				
 		
 		lista.agregarUsuario(cliente1);
 		lista.agregarUsuario(profesional1);
@@ -20,7 +31,16 @@ public class Main {
 		lista.agregarUsuario(administrativo1);
 		lista.agregarUsuario(administrativo2);
 		
+		
 		lista.imprimirUsuarios();
+		
+		//Comprobacion
+		System.out.println("Identificador: " +accidente1.getIdAccidente() +", Rut: "+accidente1.getRutCliente()+
+				", Día: "+accidente1.getDia().getDayOfMonth() +"/"+accidente1.getDia().getMonthValue()+"/"
+				+accidente1.getDia().getYear() + 
+				", Hora: "+accidente1.getHora()+", Lugar: "+accidente1.getLugar()+", Origen: "
+				+accidente1.getOrigen()+", Consecuencias: "+accidente1.getConsecuencias());		
+		
 	}
 
 }
