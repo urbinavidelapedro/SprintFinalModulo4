18bb55fbb9f092f786b9b87e1adf864d5b8664f9
Lino Cortés <lino.cortess@gmail.com>
2023-05-25 19:46:25 -0400

    Se actualiza la clase Listado

--- src/sprintfinal/Listado.java
+++ src/sprintfinal/Listado.java
@@ -5,14 +5,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Listado {
-	private LocalDate fechaNacimiento;
-	private String nombre, apellido, telefono, afp, direccion,comuna;
-	private int run,edad,sistemaSalud;
 	List<Usuario> usuarios=new ArrayList<>();
 	
-	
-	Usuario cliente=new Cliente(nombre,apellido,fechaNacimiento,run,telefono, afp, sistemaSalud,
-			direccion,comuna,edad);
 	
 	public void agregarUsuario(Usuario usuario) {
 		usuarios.add(usuario);
