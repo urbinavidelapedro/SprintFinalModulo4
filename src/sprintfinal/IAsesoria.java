7257f66ab51f265c52a390bfad09339e906951fd
Lino Cortés <lino.cortess@gmail.com>
2023-06-01 19:10:43 -0400

    Se añade Javadocs a IAsesoria

--- src/sprintfinal/IAsesoria.java
+++ src/sprintfinal/IAsesoria.java
@@ -1,5 +1,11 @@
 package sprintfinal;
-
+/**
+ * Método interfaz que declara el método analizarUsuario()
+ * @author Lino Cortés
+ * @author Felipe Toro
+ * @author Sergio Morales
+ * @author Pedro Urbina
+ */
 public interface IAsesoria {
 	void analizarUsuario();
 }
